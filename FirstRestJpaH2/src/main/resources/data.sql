9be9362cde9dc0db96e25da77e37b64e3aa01974
HARINI PARSHA <hariniparsha@gmail.com>
2024-09-15 22:09:30 +0530

    Add files via upload

---
+++ FirstRestJpaH2/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into product2 values(
+1,'TWS buds',2000.0);
+
+insert into product2 values(
+2,'KZ EDX PRO 2',3000.0);
---
+++ FirstRestJpaH2/src/main/resources/schema.sql
@@ -0,0 +1,1 @@
+create table product2 (productID int primary key,description varchar(30),price float(10));
